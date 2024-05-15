7f71026640b32dda23d7791b5fa6b45a9c52a567
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-05-15 16:23:40 +0530

    added others

---
+++ others/tutorial1.js
@@ -0,0 +1,18 @@
+// console.log("hello");
+// const ton = "hello";
+// function greet(name,study){ 
+//   console.log("hello " + name + " " + study);
+// }
+
+// greet("world","computer science");
+const wallHeight = 80;
+
+function calculaate(value){
+  console.log("The value in com is :" + value *3.54 + " cm");
+}
+
+const width = calculaate(100);
+const height = calculaate(wallHeight);
+
+const dimensions =[width,height];
+console.log (dimensions);
